053c7f0f1b77441bd0e4dafa7f9f66662fae1d1e
teyaluna <sampletextvisuals@gmx.de>
2023-10-20 04:56:15 +0200

    initial codebase added notes

--- codebase/codebase.ino
+++ codebase/codebase.ino
@@ -1,108 +1,93 @@
-/*
-  Row-Column Scanning an 8x8 LED matrix with X-Y input
+#define display_array_size 8
+// ascii 8x8 dot font
+#define data_null 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 // null char
+#define data_ascii_A 0x02,0x0C,0x18,0x68,0x68,0x18,0x0C,0x02 /*"A",0*/
+#define data_ascii_B 0x00,0x7E,0x52,0x52,0x52,0x52,0x2C,0x00 /*"B",1*/
+#define data_ascii_C 0x00,0x3C,0x66,0x42,0x42,0x42,0x2C,0x00 /*"C",2*/
+#define data_ascii_D 0x00,0x7E,0x42,0x42,0x42,0x66,0x3C,0x00 /*"D",3*/
+#define data_ascii_E 0x00,0x7E,0x52,0x52,0x52,0x52,0x52,0x42 /*"E",4*/
+#define data_ascii_F 0x00,0x7E,0x50,0x50,0x50,0x50,0x50,0x40 /*"F",5*/
+#define data_ascii_G 0x00,0x3C,0x66,0x42,0x42,0x52,0x16,0x1E /*"G",6*/
+#define data_ascii_H 0x00,0x7E,0x10,0x10,0x10,0x10,0x7E,0x00 /*"H",7*/
+#define data_ascii_I 0x00,0x00,0x00,0x7E,0x00,0x00,0x00,0x00 /*"I",8*/
 
-  This example controls an 8x8 LED matrix using two analog inputs.
-
-  This example works for the Lumex LDM-24488NI Matrix. See
-  https://sigma.octopart.com/140413/datasheet/Lumex-LDM-24488NI.pdf
-  for the pin connections.
-
-  For other LED cathode column matrixes, you should only need to change the pin
-  numbers in the row[] and column[] arrays.
-
-  rows are the anodes
-  cols are the cathodes
-  ---------
-
-  Pin numbers:
-  Matrix:
-  - digital pins 2 through 13,
-  - analog pins 2 through 5 used as digital 16 through 19
-  Potentiometers:
-  - center pins are attached to analog pins 0 and 1, respectively
-  - side pins attached to +5V and ground, respectively
-
-  created 27 May 2009
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/RowColumnScanning
-*/
-
-// 2-dimensional array of row pin numbers:
-const int row[8] = {
-  2, 7, 19, 5, 13, 18, 12, 16
+byte data_ascii[][display_array_size] = {
+  data_null,
+  data_ascii_A, 
+  data_ascii_B,
+  data_ascii_C,
+  data_ascii_D,
+  data_ascii_E,
+  data_ascii_F,
+  data_ascii_G,
+  data_ascii_H,
+  data_ascii_I,
 };
+//the pin to control ROW
+const int row1 = 2; // the number of the row pin 24
+const int row2 = 3; // the number of the row pin 23
+const int row3 = 4; // the number of the row pin 22
+const int row4 = 5; // the number of the row pin 21
+const int row5 = 17; // the number of the row pin 4
+const int row6 = 16; // the number of the row pin 3
+const int row7 = 15; // the number of the row pin 2
+const int row8 = 14; // the number of the row pin 1
+//the pin to control COl
+const int col1 = 6; // the number of the col pin 20
+const int col2 = 7; // the number of the col pin 19
+const int col3 = 8; // the number of the col pin 18
+const int col4 = 9; // the number of the col pin 17
+const int col5 = 10; // the number of the col pin 16
+const int col6 = 11; // the number of the col pin 15
+const int col7 = 12; // the number of the col pin 14
+const int col8 = 13; // the number of the col pin 13
 
-// 2-dimensional array of column pin numbers:
-const int col[8] = {
-  6, 11, 10, 3, 17, 4, 8, 9
-};
-
-// 2-dimensional array of pixels:
-int pixels[8][8];
-
-// cursor position:
-int x = 5;
-int y = 5;
-
-void setup() {
-  // initialize the I/O pins as outputs iterate over the pins:
-  for (int thisPin = 0; thisPin < 8; thisPin++) {
-    // initialize the output pins:
-    pinMode(col[thisPin], OUTPUT);
-    pinMode(row[thisPin], OUTPUT);
-    // take the col pins (i.e. the cathodes) high to ensure that the LEDS are off:
-    digitalWrite(col[thisPin], HIGH);
-  }
-
-  // initialize the pixel matrix:
-  for (int x = 0; x < 8; x++) {
-    for (int y = 0; y < 8; y++) {
-      pixels[x][y] = HIGH;
-    }
+void blank_screen() {
+  for (int i=2; i<18; i++) { 
+    digitalWrite(i, LOW);
   }
 }
 
-void loop() {
-  // read input:
-  readSensors();
+void setup () {
+  Serial.begin(9600);
+  Serial.print("lol");
+  Serial.println();
 
-  // draw the screen:
-  refreshScreen();
-}
-
-void readSensors() {
-  // turn off the last position:
-  pixels[x][y] = HIGH;
-  // read the sensors for X and Y values:
-  x = 7 - map(analogRead(A0), 0, 1023, 0, 7);
-  y = map(analogRead(A1), 0, 1023, 0, 7);
-  // set the new pixel position low so that the LED will turn on in the next
-  // screen refresh:
-  pixels[x][y] = LOW;
-}
-
-void refreshScreen() {
-  // iterate over the rows (anodes):
-  for (int thisRow = 0; thisRow < 8; thisRow++) {
-    // take the row pin (anode) high:
-    digitalWrite(row[thisRow], HIGH);
-    // iterate over the cols (cathodes):
-    for (int thisCol = 0; thisCol < 8; thisCol++) {
-      // get the state of the current pixel;
-      int thisPixel = pixels[thisRow][thisCol];
-      // when the row is HIGH and the col is LOW,
-      // the LED where they meet turns on:
-      digitalWrite(col[thisCol], thisPixel);
-      // turn the pixel off:
-      if (thisPixel == LOW) {
-        digitalWrite(col[thisCol], HIGH);
-      }
-    }
-    // take the row pin low to turn off the whole row:
-    digitalWrite(row[thisRow], LOW);
+  int i = 0;
+  for(i=2;i<18;i++)
+  {
+    pinMode(i, OUTPUT);
   }
+
+  for(i=2;i<18;i++) {
+    digitalWrite(i, LOW);
+  }
+}
+
+void loop () {
+  Serial.print("iteration"); 
+  //the row # 1 and col # 1 of the LEDs turn on 
+  digitalWrite(row1, HIGH); 
+  digitalWrite(row2, LOW); 
+  digitalWrite(row3, LOW); 
+  digitalWrite(row4, LOW); 
+  digitalWrite(row5, LOW); 
+  digitalWrite(row6, LOW); 
+  digitalWrite(row7, LOW); 
+  digitalWrite(row8, LOW); 
+  digitalWrite(col1, LOW); 
+  digitalWrite(col2, HIGH); 
+  digitalWrite(col3, HIGH); 
+  digitalWrite(col4, HIGH); 
+  digitalWrite(col5, HIGH); 
+  digitalWrite(col6, HIGH); 
+  digitalWrite(col7, HIGH); 
+  digitalWrite(col8, HIGH); 
+  delay(1000); 
+  
+  //turn off all  
+  blank_screen();
+
+  
+  delay(1000); 
 }
